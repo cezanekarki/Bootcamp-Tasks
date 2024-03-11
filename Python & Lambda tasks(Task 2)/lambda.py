20518a141ff90d98ef5160963ca81024f97d83eb
Sonu Subedi <supertrampsonu4@gmail.com>
2024-03-12 00:17:58 +0545

    Add task 2 pdf

--- Python & Lambda tasks(Task 2)/lambda.py
+++ Python & Lambda tasks(Task 2)/lambda.py
@@ -1,166 +1,224 @@
-import json
 import boto3
-from zipfile import ZipFile
+import zipfile
+import io
+import os
 import pandas as pd
 from io import BytesIO
-from datetime import datetime
-import os
 
-s3_client = boto3.client('s3')
+s3 = boto3.client('s3')
+excel_headers = {}
+
+def read_excel_from_s3(bucket):
+    try:
+        folder_path = 'excelfiles/'
+        excel_file_name = 'RxNorm_Header.xlsx'
+        key = folder_path + excel_file_name
+
+        # Download the Excel file to the /tmp directory
+        local_excel_file = '/tmp/RxNorm_Header.xlsx'
+        s3.download_file(bucket, key, local_excel_file)
+
+        # Check if the Excel file exists
+        if os.path.exists(local_excel_file):
+            print(f"Excel file downloaded to: {local_excel_file}")
+
+        # Read the Excel file into an ExcelFile object
+        excel_file = pd.ExcelFile(local_excel_file)
+
+        # Get the sheet names
+        sheet_names = excel_file.sheet_names
+        print("Sheet names:", sheet_names)
+
+        for sheets in sheet_names:
+            # Read the data from the sheet into a DataFrame
+            sheets_data = excel_file.parse(sheets, header=None)
+            headers_data = sheets_data.iloc[:, 0].tolist()
+            excel_headers[sheets] = headers_data
+
+        print(f'excel_headers dictionary for sheet {sheet_names[0]}: {excel_headers[sheet_names[0]]}')
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+def code_set_and_version_month(zip_filename, rrf_df):
+    try:
+        # Extract version month from the filename
+        version_month = os.path.splitext(zip_filename)[0].split('_')[-1]
+
+        # Convert version month to a more readable format
+        version_month = pd.to_datetime(version_month, format='%m%d%Y').strftime('%Y-%m-%d')
+        print(f"Version month: {version_month}")
+
+        # Add 'Code Set' and 'Version Month' columns to the DataFrame
+        rrf_df['Code Set'] = 'RxNorm'
+        rrf_df['Version Month'] = version_month
+
+    except Exception as e:
+        print(f"Error occurred while extracting version month: {e}")
+
+    return rrf_df
+
+def apply_header_to_rrf(file_name, rrf_df):
+    # Check if the corresponding Excel sheet exists
+    if file_name in excel_headers:
+        # Get the headers from the Excel sheet
+        excel_headers_list = excel_headers[file_name]
+        excel_headers_list = [header for header in excel_headers_list if header != 'SVER']
+
+        # Take names from the excel header list up to the length of the split DataFrame
+        excel_headers_list = excel_headers_list[:len(rrf_df.columns)]
+
+        # Set the correct header for the DataFrame
+        rrf_df.columns = excel_headers_list
+
+    return rrf_df
+
+def convert_date_format(value):
+    try:
+        # Try to parse the value into datetime format
+        parsed_date = pd.to_datetime(value, format='%Y_%m_%d').date()
+
+        # Extract only the date part
+        return parsed_date.strftime('%Y-%m-%d')
+    except ValueError:
+        if value == '2020':
+            return '2020-01-01'
+        elif value == '5.0_2024_01_04':
+            # Remove the float value and parse the remaining string
+            return convert_date_format('2024_01_04')
+        elif value == '2020AA':
+            return '2024-01-02'
+        elif value == '20AA_240205F':
+            return '2024-02-05'
+        else:
+            return value
+
+def update_nato_date(value):
+    try:
+        # Attempt to parse the value using the first date format
+        parsed_date = pd.to_datetime(value, format='%m/%d/%Y %I:%M:%S %p').date()
+    except ValueError:
+        try:
+            # If the first format fails, attempt to parse using the second date format
+            parsed_date = pd.to_datetime(value, format='%d-%b-%y').date()
+        except ValueError:
+            # If both formats fail, return None or handle the error appropriately
+            return None  # Or handle the error appropriately
+
+    # Check if the parsed_date is NaT
+    if pd.isnull(parsed_date):
+        return '0000-00-00'  # Replace NaT with '0000-00-00'
+    else:
+        # Extract only the date part and return it in the desired format
+        return parsed_date.strftime('%Y-%m-%d')
+
+def process_date_columns(file_name, rrf_df):
+    date_columns = ['VSTART', 'VEND', 'CREATED_TIMESTAMP', 'UPDATED_TIMESTAMP', 'LAST_RELEASED']
+
+    for column in date_columns:
+        if column in rrf_df.columns:
+            if file_name == 'RXNSAB':
+                # Apply the conversion function to each value in the column
+                rrf_df[column] = rrf_df[column].apply(convert_date_format)
+
+                # Extract year from the VSTART column after date conversion and save it directly as a string
+                rrf_df['SVER'] = pd.to_datetime(rrf_df['VSTART'], format='%Y-%m-%d').dt.year.astype(str)
+
+                # Reorder the columns to place 'SVER' before 'VSTART'
+                sver_index = rrf_df.columns.get_loc('SVER')
+                vstart_index = rrf_df.columns.get_loc('VSTART')
+
+                # Remove 'SVER' from its original position
+                column_sver = rrf_df.pop('SVER')
+
+                # Insert 'SVER' after 'SF', before 'VSTART'
+                if sver_index < vstart_index:
+                    rrf_df.insert(vstart_index - 1, 'SVER', column_sver)
+                elif sver_index > vstart_index:
+                    rrf_df.insert(vstart_index, 'SVER', column_sver)
+
+            if file_name == 'RXNATOMARCHIVE':
+                rrf_df[column] = rrf_df[column].apply(update_nato_date)
+
+    return rrf_df
+
+def save_as_txt_file(rrf_df, file_name, bucket_name):
+    # Construct the filename for the output text file
+    transformation_folder = 'transformation/'
+
+    # Convert DataFrame to CSV format in memory
+    csv_buffer = io.StringIO()
+    rrf_df.to_csv(csv_buffer, sep=',', index=False)
+
+    # Upload the CSV buffer to S3
+    s3_key = transformation_folder + file_name + '.csv'
+    s3.put_object(Bucket=bucket_name, Key=s3_key, Body=csv_buffer.getvalue())
+    print(f"Transformed data saved to: s3://{bucket_name}/{s3_key}")
+
+def read_and_relocate_rrf_files(s3, bucket, key):
+    try:
+        zip_response = s3.get_object(Bucket=bucket, Key=key)
+        zip_data = zip_response['Body'].read()
+        zip_filename = os.path.basename(key)
+        print(zip_filename)
+
+        # Wrap the zip data in a BytesIO object
+        zip_file = BytesIO(zip_data)
+        file_path = 'rrf'
+        unzipped_folder = 'unzipped/'
+
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            for file_info in zip_ref.infolist():
+                if file_info.filename.startswith(file_path) and not file_info.filename.endswith('/'):
+                    filename = os.path.basename(file_info.filename)
+                    print(f"The {filename} is read from zip file.")
+
+                    with zip_ref.open(file_info) as source_file:
+                        file_content = source_file.read().decode('utf-8')
+
+                        if file_content.endswith('|'):
+                            file_content = file_content[:-1]
+
+                        file_content_io = io.StringIO(file_content)
+                        rrf_df = pd.read_csv(file_content_io, delimiter='|', header=None)
+                        rrf_df = rrf_df.iloc[:, :-1]
+
+                        print(f"Row count before transformation: {rrf_df.shape[0]}")
+                        file_name = os.path.splitext(filename)[0]
+                        apply_header_to_rrf(file_name, rrf_df)
+                        rrf_df = process_date_columns(file_name, rrf_df)
+                        code_set_and_version_month(zip_filename, rrf_df)
+                        print(f"Row count of {file_name} after transformation: {rrf_df.shape[0]}")
+
+                        pd.set_option('display.max_columns', None)
+                        print(rrf_df.head(5))
+
+                        # Save the transformed DataFrame to a text file
+                        save_as_txt_file(rrf_df, file_name, bucket)
+
+                    # Upload the unzipped file to the 'unzipped' folder
+                    unzipped_key = unzipped_folder + filename
+                    s3.put_object(Bucket=bucket, Key=unzipped_key, Body=file_content)
+                    print(f"Unzipped file saved to: s3://{bucket}/{unzipped_key}")
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
 
 def lambda_handler(event, context):
     try:
-        # Extract bucket and key information from the S3 event
-        bucket = event['Records'][0]['s3']['bucket']['name']
-        key = event['Records'][0]['s3']['object']['key']
-        
-        if key.endswith('.xlsx'):
-            handle_excel_upload(bucket, key)
-        elif key.endswith('.zip'):
-            handle_zip_upload(bucket, key)
+        # Check if 'Records' key is present in the event dictionary
+        # if 'Records' in event:
+            # Extract bucket and key from the S3 event
+            # bucket = event['Records'][0]['s3']['bucket']['name']
+            # key = event['Records'][0]['s3']['object']['key']
 
-        # Read the zip file directly from S3
-        zip_file_obj = s3_client.get_object(Bucket=bucket, Key=key)
-        zip_file_content = BytesIO(zip_file_obj['Body'].read())
-
-        # Perform RxNorm transformation
-        rxnorm_transformation(zip_file_content, bucket, key)
+        bucket= 'pythonlambdabucket1'
+        key = 'zipfiles/RxNorm_full_02052024.zip'
+        # Rest of your code that uses bucket and key
+        read_excel_from_s3(bucket)
+        read_and_relocate_rrf_files(s3, bucket, key)
+        # else:
+        #     print("S3 event structure is not as expected. Missing 'Records' key.")
 
     except Exception as e:
-        print(f"An error occurred: {e}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Error occurred during transformation')
-        }
-
-def rxnorm_transformation(zip_file_content, bucket, zip_key):
-    try:
-        print("Transformation Started")
-        before_conversion_dict = {}
-        after_conversion_dict = {}
-        transformed_data = {}  # Placeholder for your transformed data
-
-        # Read the zip file directly from memory
-        with ZipFile(zip_file_content, 'r') as zip_ref:
-            rrf_files = [file[len('rrf/'):] for file in zip_ref.namelist() if file.startswith('rrf/')]
-            sorted_rrf_files = sorted(rrf_files)
-
-            if sorted_rrf_files:
-                # Find the Excel file dynamically
-                excel_key = find_file_in_bucket(bucket, 'RxNorm_Header.xlsx')
-
-                if not excel_key:
-                    print("Excel file not found in the bucket.")
-                    return
-
-                excel_file_obj = s3_client.get_object(Bucket=bucket, Key=excel_key)
-                excel_file_content = BytesIO(excel_file_obj['Body'].read())
-                
-                for file in sorted_rrf_files:
-                    # Read CSV files and count rows before conversion
-                    df = read_csv_from_zip(zip_ref, file)
-                    before_conversion_dict[file] = df.shape[0]
-
-                # Read the sheet names from the Excel file
-                excelfile = pd.ExcelFile(excel_file_content)
-                sheet_names = excelfile.sheet_names
-                sheet_names.sort()
-                
-                # print(sheet_names,"names")
-
-                # Get the date from the zip file key
-                zip_file_date = extract_date_from_zip_path(zip_key)
-                # print(zip_file_date,"date")
-
-                for i, file in enumerate(sorted_rrf_files):
-                    # Read Excel sheets, perform transformations, and count rows after conversion
-                    df_dict = read_excel_sheet(excel_file_content, sheet_name=sheet_names[i])
-                    filet = zip_ref.open(f'rrf/{sorted_rrf_files[i]}')
-                    df = pd.read_table(filet, sep='|', header=None, low_memory=False)
-                    df['VERSION_MONTH'] = [zip_file_date] * df.shape[0]
-                    df.columns = df_dict
-                    df['CODE_SET'] = ['RXNORM'] * df.shape[0]
-                    format_dates(df)
-                    # print(df_dict,"df_dict")
-                    after_conversion_dict[sheet_names[i]] = df.shape[0]
-
-                    # Save DataFrame to a text file
-                    transformed_data[sheet_names[i]] = df
-                    # print(df.head(2))
-                    
-        # print("Before Conversion:", before_conversion_dict)
-        # print("After Conversion:", after_conversion_dict)
-
-        # Your existing code to print the final table
-        before_conversion_dict = {key.replace('.RRF', ''): value for key, value in before_conversion_dict.items()}
-        # print(before_conversion_dict,"before")
-        before_df = pd.DataFrame(list(before_conversion_dict.items()), columns=['File Name', 'Before Conversion'])
-        after_df = pd.DataFrame(list(after_conversion_dict.items()), columns=['File Name', 'After Conversion'])
-        merged_df = pd.merge(before_df, after_df, on='File Name')
-        print(merged_df)
-# 
-        # # Save the transformed text files back to S3
-        for sheet_name, df in transformed_data.items():
-            save_txt_file(df, bucket, sheet_name)
-            print(f"{sheet_name}.txt is saved")
-
-        print("Transformation Completed")
-    except Exception as e:
-        print(f"An error occurred during transformation: {e}")
-        raise e
-def handle_excel_upload(bucket, key):
-    # Logic to handle .xlsx file upload
-    # You can update your state, database, or any mechanism to track the uploaded .xlsx file
-    print(f".xlsx file uploaded: {key}")
-    
-def handle_zip_upload(bucket, key):
-    # Logic to handle .zip file upload
-    # You can update your state, database, or any mechanism to track the uploaded .zip file
-    print(f".zip file uploaded: {key}")
-
-def find_file_in_bucket(bucket, file_name):
-    try:
-        response = s3_client.list_objects_v2(Bucket=bucket, Prefix=file_name)
-        for obj in response.get('Contents', []):
-            if obj['Key'] == file_name:
-                # print(obj['Key'])
-                return obj['Key']
-        return None
-    except Exception as e:
-        print(f"An error occurred while finding the file: {e}")
-        raise e
-
-def read_excel_sheet(excel_file_content, sheet_name):
-    df_dict = pd.read_excel(excel_file_content, sheet_name=sheet_name, header=None)
-    return df_dict[1].to_list()
-
-def extract_date_from_zip_path(zip_path):
-    file_name = os.path.basename(zip_path)
-    digits = ''.join(filter(str.isdigit, file_name))
-
-    if len(digits) >= 8:
-        month = digits[:2]
-        day = digits[2:4]
-        year = digits[4:8]
-
-        date_str = f"{month}{day}{year}"
-        date_obj = datetime.strptime(date_str, "%m%d%Y")
-        formatted_date = date_obj.strftime("%Y-%m-%d")
-
-        return formatted_date
-    else:
-        return None
-
-def format_dates(df):
-    date_cols = [col for col in df.columns if 'DATE' in col or 'RXNORM_TERM_DT' in col]
-    for col in date_cols:
-        df[col] = pd.to_datetime(df[col], format="%Y-%m-%d", errors='coerce').dt.strftime("%Y-%m-%d")
-
-def read_csv_from_zip(zip_ref, file):
-    # Function to read a CSV file from a Zip archive
-    return pd.read_csv(zip_ref.open(f'rrf/{file}'), sep='|', header=None, encoding="utf-8", low_memory=False)
-
-def save_txt_file(df, bucket, sheet_name):
-    # Function to save a DataFrame to a text file and upload to S3
-    txt_file_content = df.to_csv(index=False, sep=',').encode('utf-8')
-    s3_client.put_object(Body=txt_file_content, Bucket=bucket, Key=f'allfiles/{sheet_name}.txt')
+        print(f"Error occurred: {e}")
